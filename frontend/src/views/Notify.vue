e5d0ff2ad530d251102af77b50de25808d741540
zhoulin2 <zhoulin2@asiainfo.com>
2020-02-02 21:20:35 +0800

    [新增] 初始化前端项目

---
+++ frontend/src/views/Notify.vue
@@ -0,0 +1,30 @@
+<template>
+    <table>
+        <tr>
+            <th>编号</th>
+            <th>内容</th>
+        </tr>
+        <tr v-for="(item, index) in notifies" :key="index">
+            <td>{{item.id}}</td>
+            <td>{{item.content}}</td>
+        </tr>
+    </table>
+</template>
+
+<script>
+    export default {
+        name: "Notify",
+        data() {
+            return {
+                notifies: [
+                    {id: 1, content: '123'},
+                    {id: 2, content: '123123'},
+                ],
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
